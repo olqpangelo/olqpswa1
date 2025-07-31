3c369ed744d18097a84cfe3c0cf61edd27b0cbac
olqpangelo <akang@olqpva.org>
2025-07-30 20:49:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function fetchUnifiLogs() {
-  const response = await fetch('https://your-backend-api/unifi-logs');
+  const response = await fetch('https://unifi.ui.com/consoles/28704E20271C0000000008237494000000000891E4970000000066784270:538230538/network/default/syslog/main');
   const data = await response.json();
   document.getElementById("logOutput").textContent = JSON.stringify(data, null, 2);
 }
